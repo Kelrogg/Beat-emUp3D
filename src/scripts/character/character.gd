318263e66f827f2e397119524e3afc28ad75ec95
beta442 <grandoblivioniv@gmail.com>
2022-08-26 18:11:09 +0300

    now uses finite state machine for animation changing handle

--- src/scripts/character/character.gd
+++ src/scripts/character/character.gd
@@ -1,56 +1,67 @@
-extends KinematicBody
+class_name Character
+extends AnimKinematicBody
 
-export (int) var run_speed = 1
-export (int) var jump_speed = 10
-export (int) var gravity = -1
+onready var states := $MovementStateManager
 
-var velocity := Vector3.ZERO
-var v_speed: float = 0
-var jumping := false
+func _ready() -> void:
+	states.init(self)
+
+func _unhandled_input(event: InputEvent) -> void:
+	states.input(event)
 
 func _physics_process(delta: float) -> void:
-#	print('z - %d' % z_index)	# оставь строчку - мне понравилась
-	
-	velocity = Vector3.ZERO
-	get_input()
-	
-	var jump := Input.is_action_just_pressed("ui_jump")
+	states.physics_process(delta)
 
-	if jump and is_on_floor():
-		#jumping = true
-		v_speed += jump_speed
+func _process(delta: float) -> void:
+	states.process(delta)
 
-	#if jumping:
-	velocity.y += v_speed
-	v_speed += gravity
+#var velocity := Vector3.ZERO
+#var v_speed: float = 0
+#var jumping := false
 
-	move_and_slide(velocity, Vector3(0,1,0), true)
-	#translate(velocity * delta)
-	
-	if is_on_floor():
-		#jumping = false
-		#translation.y = 0
-		v_speed = 0
-
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "run"
-		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment.
-		$AnimatedSprite.flip_h = velocity.x < 0
-	if velocity.x == 0:
-		$AnimatedSprite.animation = "idle"
-
-	$AnimatedSprite.translation.y = translation.z
-
-func get_input():
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.z -= 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.z += 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * run_speed
+#func _physics_process(delta: float) -> void:
+##	print('z - %d' % z_index)	# оставь строчку - мне понравилась
+#
+#	velocity = Vector3.ZERO
+#	get_input()
+#
+#	var jump := Input.is_action_just_pressed("ui_jump")
+#
+#	if jump and is_on_floor():
+#		#jumping = true
+#		v_speed += jump_speed
+#
+#	#if jumping:
+#	velocity.y += v_speed
+#	v_speed += gravity
+#
+#	move_and_slide(velocity, Vector3(0,1,0), true)
+#	#translate(velocity * delta)
+#
+#	if is_on_floor():
+#		#jumping = false
+#		#translation.y = 0
+#		v_speed = 0
+#
+#	if velocity.x != 0:
+#		$AnimatedSprite.animation = "run"
+#		$AnimatedSprite.flip_v = false
+#		# See the note below about boolean assignment.
+#		$AnimatedSprite.flip_h = velocity.x < 0
+#	if velocity.x == 0:
+#		$AnimatedSprite.animation = "idle"
+#
+#	$AnimatedSprite.translation.y = translation.z
+#
+#func get_input():
+#	if Input.is_action_pressed("ui_right"):
+#		velocity.x += 1
+#	if Input.is_action_pressed("ui_left"):
+#		velocity.x -= 1
+#	if Input.is_action_pressed("ui_down"):
+#		velocity.z -= 1
+#	if Input.is_action_pressed("ui_up"):
+#		velocity.z += 1
+#
+#	if velocity.length() > 0:
+#		velocity = velocity.normalized() * run_speed
